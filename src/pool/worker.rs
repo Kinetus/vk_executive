9cd071c07ff693b0109c67b12333c40c8e74b642
Trard <trardone@gmail.com>
2022-08-15 17:40:45 +0300

    refactoring

--- src/pool/worker.rs
+++ src/pool/worker.rs
@@ -3,11 +3,12 @@ use itertools::izip;
 use crate::types::{Error as VkError, Result as VkResult};
 use serde_json::value::Value;
 
+use tokio::sync::oneshot;
 use tokio::task::JoinHandle;
 use tokio::time::{sleep, Duration};
 
-use super::execute_manager::{ExecuteManager, Event};
-use super::{Instance, Message, MethodWithSender};
+use super::execute_manager::{Event, ExecuteManager};
+use super::{Instance, Message, Method, MethodWithSender};
 
 pub struct Worker {
     #[allow(dead_code)]
@@ -30,88 +31,11 @@ impl Worker {
 
                 match receiver.recv() {
                     Ok(message) => match message {
-                        Message::NewMethod(MethodWithSender {
-                            method,
-                            sender: oneshot_sender,
-                        }) => {
-                            let url = format!("https://api.vk.com/method/{}", method.name);
-                            let req = client
-                                .post(url)
-                                .header("Content-Length", 0)
-                                .query(&method.params)
-                                .query(&[
-                                    ("access_token", instance.token()),
-                                    ("v", "5.103".to_string()),
-                                ])
-                                .send();
-
-                            tokio::spawn(async move {
-                                oneshot_sender
-                                    .send(req.await.unwrap().json().await.unwrap())
-                                    .unwrap();
-                            });
+                        Message::NewMethod(MethodWithSender { method, sender }) => {
+                            Worker::handle_method(method, sender, &client, &instance)
                         }
                         Message::NewExecute(methods, senders) => {
-                            let execute = ExecuteManager::compile_execute(methods);
-
-                            let url = format!("https://api.vk.com/method/execute");
-                            let req = client
-                                .post(url)
-                                .header("Content-Length", 0)
-                                .query(&[("code", execute)])
-                                .query(&[
-                                    ("access_token", instance.token()),
-                                    ("v", "5.103".to_string()),
-                                ])
-                                .send();
-
-                            tokio::spawn(async move {
-                                let mut raw_response: Value =
-                                    req.await.unwrap().json().await.unwrap();
-
-                                let execute_errors_raw =
-                                    if let Value::Object(ref mut map) = raw_response {
-                                        map.remove("execute_errors")
-                                    } else {
-                                        None
-                                    };
-
-                                let mut execute_errors: Vec<VkError> = Vec::new();
-
-                                if let Some(execute_errors_value) = execute_errors_raw {
-                                    execute_errors =
-                                        serde_json::from_value(execute_errors_value).unwrap();
-                                }
-
-                                let response: VkResult<Value> =
-                                    serde_json::from_value(raw_response).unwrap();
-
-                                match response {
-                                    VkResult::response(responses) => {
-                                        let responses: Vec<Value> =
-                                            serde_json::from_value(responses).unwrap();
-
-                                        for (sender, response) in izip!(senders, responses) {
-                                            if let Some(bool) = response.as_bool() {
-                                                if bool == false {
-                                                    sender
-                                                        .send(VkResult::error(
-                                                            execute_errors.remove(0),
-                                                        ))
-                                                        .unwrap();
-                                                }
-                                            } else {
-                                                sender.send(VkResult::response(response)).unwrap();
-                                            }
-                                        }
-                                    }
-                                    VkResult::error(error) => {
-                                        for sender in senders {
-                                            sender.send(VkResult::error(error.clone())).unwrap();
-                                        }
-                                    }
-                                }
-                            });
+                            Worker::handle_execute(methods, senders, &client, &instance)
                         }
                         Message::Terminate => {
                             break;
@@ -130,5 +54,90 @@ impl Worker {
             thread: Some(thread),
             id,
         }
+    }
+
+    fn handle_method(
+        method: Method,
+        sender: oneshot::Sender<VkResult<Value>>,
+        client: &reqwest::Client,
+        instance: &Instance,
+    ) {
+        let url = format!("https://api.vk.com/method/{}", method.name);
+        let req = client
+            .post(url)
+            .header("Content-Length", 0)
+            .query(&method.params)
+            .query(&[
+                ("access_token", instance.token()),
+                ("v", "5.103".to_string()),
+            ])
+            .send();
+
+        tokio::spawn(async move {
+            sender
+                .send(req.await.unwrap().json().await.unwrap())
+                .unwrap();
+        });
+    }
+
+    fn handle_execute(
+        methods: Vec<Method>,
+        senders: Vec<oneshot::Sender<VkResult<Value>>>,
+        client: &reqwest::Client,
+        instance: &Instance,
+    ) {
+        let execute = ExecuteManager::compile_execute(methods);
+
+        let url = format!("https://api.vk.com/method/execute");
+        let req = client
+            .post(url)
+            .header("Content-Length", 0)
+            .query(&[("code", execute)])
+            .query(&[
+                ("access_token", instance.token()),
+                ("v", "5.103".to_string()),
+            ])
+            .send();
+
+        tokio::spawn(async move {
+            let mut raw_response: Value = req.await.unwrap().json().await.unwrap();
+
+            let execute_errors_raw = if let Value::Object(ref mut map) = raw_response {
+                map.remove("execute_errors")
+            } else {
+                None
+            };
+
+            let mut execute_errors: Vec<VkError> = Vec::new();
+
+            if let Some(execute_errors_value) = execute_errors_raw {
+                execute_errors = serde_json::from_value(execute_errors_value).unwrap();
+            }
+
+            let response: VkResult<Value> = serde_json::from_value(raw_response).unwrap();
+
+            match response {
+                VkResult::response(responses) => {
+                    let responses: Vec<Value> = serde_json::from_value(responses).unwrap();
+
+                    for (sender, response) in izip!(senders, responses) {
+                        if let Some(bool) = response.as_bool() {
+                            if bool == false {
+                                sender
+                                    .send(VkResult::error(execute_errors.remove(0)))
+                                    .unwrap();
+                            }
+                        } else {
+                            sender.send(VkResult::response(response)).unwrap();
+                        }
+                    }
+                }
+                VkResult::error(error) => {
+                    for sender in senders {
+                        sender.send(VkResult::error(error.clone())).unwrap();
+                    }
+                }
+            }
+        });
     }
 }
