5bbaa3a12636a7ec7b826b258a7b050a8aa6926b
Trard <trardone@gmail.com>
2022-09-22 18:27:31 +0300

    added 1000 test

--- tests/pool.rs
+++ tests/pool.rs
@@ -133,3 +133,28 @@ async fn ten_tasks_three_workers() {
         assert_eq!(res[0], get_users()[index]);
     }
 }
+
+#[tokio::test(flavor = "multi_thread")]
+async fn one_thousand_tasks_ten_workers() {
+    let result = dotenv();
+
+    dbg!(result);
+    
+    let instances = Instance::from_tokens(env::var("tokens").unwrap().split(",").take(10)).unwrap();
+
+    let pool = InstancePool::new(instances.into_iter());
+
+    let mut vec = Vec::new();
+
+    for i in 1..1001 {
+        let params = Params::try_from(PairsArray([("user_id", i)])).unwrap();
+
+        vec.push(pool.run(Method::new("users.get", params)));
+    }
+
+    let responses = join_all(vec).await;
+
+    for res in responses {
+        println!("{:?}", res)
+    }
+}
